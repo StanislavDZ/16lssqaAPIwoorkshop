8867513769a9cd60d84ee2d91e5083368f77e0c0
StanislavDZ <87750213+StanislavDZ@users.noreply.github.com>
2022-04-19 22:38:03 +0300

    http://demowebshop.tricentis.com 17lss + allure

--- src/test/java/reqres/WorkShopTest.java
+++ src/test/java/reqres/WorkShopTest.java
@@ -1,23 +1,52 @@
 package reqres;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static listenters.CustomAllureListener.withCustomTemplates;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 
 public class WorkShopTest {
 
+    @BeforeAll
+    static void setUp() {
+        RestAssured.baseURI = "http://demowebshop.tricentis.com/";
+    }
+
     @Test
     @DisplayName("Добавить товар в корзину")
     void addJewelryToCart() {
         given()
+                .filter(withCustomTemplates())
                 .contentType("application/x-www-form-urlencoded; charset=UTF-8")
-                .cookie("Nop.customer=16d5f2e3-1840-400f-9e7a-cf319a2733fd;")
+                //.cookie("Nop.customer=16d5f2e3-1840-400f-9e7a-cf319a2733fd;")
                 .body("addtocart_14.EnteredQuantity=1")
                 .when()
-                .post("http://demowebshop.tricentis.com/addproducttocart/catalog/14/1/1")
+                .post("addproducttocart/catalog/14/1/1")
                 .then()
+                .log().all()
+                .body("success", is(true))
+                .body("message", is("The product has been added to your <a href=\"/cart\">shopping cart</a>"))
+                .body("updatetopcartsectionhtml", is(notNullValue()))
+                .statusCode(200);
+    }
+
+
+    @Test
+    @DisplayName("Добавить товар в корзину")
+    void addJewelry1ToCart() {
+        given()
+                .filter(withCustomTemplates())
+                .contentType("application/x-www-form-urlencoded; charset=UTF-8")
+                //.cookie("Nop.customer=16d5f2e3-1840-400f-9e7a-cf319a2733fd;")
+                .body("addtocart_14.EnteredQuantity=1")
+                .when()
+                .post("addproducttocart/catalog/14/1/2")
+                .then()
+                .log().all()
                 .body("success", is(true))
                 .body("message", is("The product has been added to your <a href=\"/cart\">shopping cart</a>"))
                 .body("updatetopcartsectionhtml", is(notNullValue()))
@@ -28,12 +57,14 @@ public class WorkShopTest {
     @DisplayName("Добавить товар в вишлист")
     void addJewelryToWishList() {
         given()
+                .filter(withCustomTemplates())
                 .contentType("application/x-www-form-urlencoded; charset=UTF-8")
-                .cookie("Nop.customer=16d5f2e3-1840-400f-9e7a-cf319a2733fd;")
+                //.cookie("Nop.customer=16d5f2e3-1840-400f-9e7a-cf319a2733fd;")
                 .body("addtocart_14.EnteredQuantity=1")
                 .when()
-                .post("http://demowebshop.tricentis.com/addproducttocart/details/14/2")
+                .post("addproducttocart/details/14/2")
                 .then()
+                .log().all()
                 .body("success", is(true))
                 .body("message", is("The product has been added to your <a href=\"/wishlist\">wishlist</a>"))
                 .body("updatetopwishlistsectionhtml", is(notNullValue()))
